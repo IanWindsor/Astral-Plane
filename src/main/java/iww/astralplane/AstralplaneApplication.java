7e71b3b552bd826732b139de623b912789db20d1
Ian Wheeler <Ian.W.Wheeler@gmail.com>
2018-08-20 17:20:57 -0700

    Initializing Astral-Plane project
    Initializing Astral-Plane, a web application using Spring Boot.

---
+++ src/main/java/iww/astralplane/AstralplaneApplication.java
@@ -0,0 +1,12 @@
+package iww.astralplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstralplaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstralplaneApplication.class, args);
+	}
+}
